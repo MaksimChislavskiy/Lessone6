f979a2e5c4537293158f124f7c32f7af1835c87f
MaksimChislavskiy <maxwell884884@gmail.com>
2025-05-24 17:07:49 +0300

    final class Hero

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ class Hero():
         self.attack_power = attack_power
 
     def attack(self, other):
-        ...
+        other.health -= self.attack_power
+        print(f'{self.name} атакует {other.name} и наносит {self.attack_power} урона')
+
 
     def is_alive(self):
-        ...
+        return self.health > 0
+
+
+hero1 = Hero('Герой 1')
+hero2 = Hero('Герой 2')
+
+hero1.attack(hero2)
